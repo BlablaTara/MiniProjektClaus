1524c2b31a3eeb7b279828f077f564edbff7b29d
Anders <andersravn94@gmail.com>
2024-03-15 14:29:44 +0100

    Bugfixes

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello");
-        System.out.println("from the other Emma");
-        System.out.println("I must have called a thousand times");
-        System.out.println("To tell you I'm sorry");
-        System.out.println("For everything that I've done");
-        System.out.println("But when I call you never");
-        System.out.println("Seem to be home");
-        System.out.println("Hello from the outside");
-        System.out.println("At least I can say that I've tried");
-        System.out.println("To tell you I'm sorry");
-        System.out.println("For breaking your heart");
-        System.out.println("But it don't matter, it clearly");
+
     }
 }
