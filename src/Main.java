cb3424885db0de369a70b6868e550377dc7361d3
Anders <andersravn94@gmail.com>
2024-03-15 14:25:39 +0100

    more lyrics

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
         System.out.println("Hello");
         System.out.println("from the other Emma");
+        System.out.println("I must have called a thousand times");
+        System.out.println("To tell you I'm sorry");
+        System.out.println("For everything that I've done");
+        System.out.println("But when I call you never");
+        System.out.println("Seem to be home");
+        System.out.println("Hello from the outside");
+        System.out.println("At least I can say that I've tried");
+        System.out.println("To tell you I'm sorry");
+        System.out.println("For breaking your heart");
+        System.out.println("But it don't matter, it clearly");
     }
 }
